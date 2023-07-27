8daf28fdb9c5502871533ada6e11d994daf3cb21
aj <ahmed24633@gmail.com>
2023-07-27 10:50:56 +0300

    function def comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function makes a matrix and caches its mean
 
 makeCacheMatrix <- function(x = matrix()) {
 
